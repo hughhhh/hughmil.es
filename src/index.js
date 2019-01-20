f0ca5dfbd782e5cea24df7a53c79cb11e4c30448
Hugh A. Miles II <hughmil3s@gmail.com>
2019-01-19 19:03:40 -0800

    Refactor code into functions

--- src/index.js
+++ src/index.js
@@ -1,47 +1,89 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import autoBind from "auto-bind";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
+const projects = [{ name: "St. George Learning Center" }];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+  }
+
+  renderProjects() {
+    return (
       <div
         style={{
-          display: "flex",
-          justifyContent: "flex-start",
-          flexDirection: "row",
-          backgroundColor: "green"
+          width: "25%"
         }}
       >
-        <div style={{ justifyContent: "flex-start" }}>
-          <div style={{ margin: "10px 0" }}>
-            Hugh Miles
-            <br />
-            Software Engineer
-          </div>
-          <div>
-            I am Hugh Miles, a 26 year old Software engineer living in San
-            Francisco working for the best transportation company in the world
-            Lyft. I've been coding for over 5 years and have worked on multiple
-            projects ranging from landing pages and data pipelines. To see some
-            of my work feel free to explore the side-B of the site.
-          </div>
-          <div>hugh@hacksnextdoor.com</div>
-          <div>Linkedin Instagram Github Medium</div>
+        <div>Projects</div>
+        {projects.map(item => (
+          <div>{item.name}</div>
+        ))}
+      </div>
+    );
+  }
+
+  renderBio() {
+    return (
+      <div style={{ width: "25%" }}>
+        <div className="padding-sm">
+          Hugh Miles
+          <br />
+          Software Engineer
         </div>
-        <div
-          style={{ justifyContent: "flex-start", backgroundColor: "blue" }}
-        />
-        <div style={{ justifyContent: "flex-start", backgroundColor: "red" }}>
-          Projects
+        <div className="padding-sm">
+          I am Hugh Miles, a 26 year old Software engineer living in San
+          Francisco working for the best transportation company in the world
+          Lyft. I've been coding for over 5 years and have worked on multiple
+          projects ranging from landing pages and data pipelines. To see some of
+          my work feel free to explore the side-B of the site.
         </div>
-        <div style={{ justifyContent: "flex-start", backgroundColor: "blue" }}>
-          Project Details
+        <div className="padding-sm">hugh@hacksnextdoor.com</div>
+        <div className="padding-sm">
+          <div>Linkedin</div>
+          <div>Medium </div>
+          <div>Github </div>
+          <div>Instagram</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  renderProjectDetails() {
+    return (
+      <div
+        style={{
+          justifyContent: "flex-start",
+          width: "50%"
+        }}
+      >
+        Project Details
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            flexDirection: "row",
+            width: "100%"
+          }}
+        >
+          {this.renderBio()}
+          {this.renderProjects()}
+          {this.renderProjectDetails()}
+        </div>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
